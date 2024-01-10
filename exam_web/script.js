e825c2c4c3852ccc4a463f9785c4ccd4367105f2
Sergey <90379389+SergeyKlimenkov221-331@users.noreply.github.com>
2024-01-11 02:09:22 +0300

    Add files via upload

---
+++ exam_web/script.js
@@ -0,0 +1,5 @@
+
+window.onload = function() {
+    spisokMarshrytov.open('GET', 'http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=49fa4a62-33b2-4d5a-bcf6-b09b6600974f', true);
+    spisokMarshrytov.send();
+}
